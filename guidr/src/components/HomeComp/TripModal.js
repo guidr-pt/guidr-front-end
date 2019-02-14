6886f490a034198b78481089fa24bfb828f98e7f
Trip1701 <bj2336@email.vccs.edu>
2019-02-14 10:53:22 -0500

    Modal Display-Edit Toggle Added

--- guidr/src/components/HomeComp/TripModal.js
+++ guidr/src/components/HomeComp/TripModal.js
@@ -8,45 +8,63 @@ class TripModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
+      edit: false,
     };
-
-    this.toggle = this.toggle.bind(this);
   }
 
-  toggle() {
+  toggle = () => {
     this.setState(prevState => ({
       modal: !prevState.modal
+    }));
+  }
+
+  editMode = () => {
+    this.setState(prevState => ({
+      edit: !prevState.edit
     }));
   }
 
   render() {
     const externalCloseBtn = <button className="close" style={{ position: 'absolute', top: '15px', right: '15px' }} onClick={this.toggle}>&times;</button>;
     const trip = this.props.trip;
+    const editMode = this.state.edit;
+    const checkBoxPrivate = <div>
+                              <input type='checkbox' /> {String(trip.private)}
+                            </div>
+    console.log(editMode)
 
     return (
       <div className='modal-container'>
         <Button color="danger" onClick={this.toggle}>View</Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} external={externalCloseBtn}>
-          <ModalHeader>Modal title</ModalHeader>
+
+          <ModalHeader>{ editMode ? <input placeholder={trip.name}/> : trip.name }</ModalHeader>
+
           <ModalBody>
-            <div>
-              <h2>{trip.name}</h2>
-              <h3>{trip.description}</h3>
-              <img src={trip.img} alt={trip.name}/>
-              <h5>{trip.type}</h5>
+            <div className='modal-info'>
+              <img src={trip.img} alt={trip.name} id='modal-image'/>
 
               <div>
-                <input type='checkbox' /> {String(trip.private)}
+                { editMode ? <input placeholder={trip.name}/> : <h2>{trip.name}</h2> }
+                { editMode ? <input placeholder={trip.name}/> : <p>{trip.description}</p> }
+
+                { editMode ? checkBoxPrivate : <p>{String(trip.private)}</p> }
               </div>
 
-              <p>{trip.duration}</p>
-              <p>{trip.date}</p>
+
             </div>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            <div>
+              { editMode ? <input placeholder={trip.name}/> : <p>{trip.duration}</p> }
+              { editMode ? <input placeholder={trip.name}/> : <p>{trip.date}</p> }
+            </div>
+
+            <div>
+              <Button color="primary" onClick={this.editMode}>{ editMode ? 'Save' : 'Edit'}</Button>
+              <Button color="secondary" onClick={this.toggle}>Close</Button>
+            </div>
           </ModalFooter>
         </Modal>
       </div>
