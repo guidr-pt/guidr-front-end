c979d579842882093aacb33de451eca65cd3a2f0
Trip4077 <bj2336@email.vccs.edu>
2019-03-07 22:02:52 -0500

    Edit and Delete Trips tested, Trip Cards now display image

--- src/components/TripComponents/TripCard.js
+++ src/components/TripComponents/TripCard.js
@@ -2,10 +2,11 @@ import React from 'react';
 import TripModal from './TripModal';
 
 const TripCard = props => {
+  console.log('TRIps,', props.trip )
   return(
     <div className='tripgrid__card'>
       <h2>{props.trip.title}</h2>
-
+      <img src={props.trip.image} alt={props.trip.description} />
       <TripModal trip={props.trip} />
     </div>
   );
